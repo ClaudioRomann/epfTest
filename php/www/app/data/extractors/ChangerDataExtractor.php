dd4a06dfd546d77000215fcf466bb36c886e1f0f
Claudio Roman <claudio.roman@cn-consult.eu>
2024-01-17 11:12:55 +0100

    Config parser use added and data extractors refactoring

---
+++ php/www/app/data/extractors/ChangerDataExtractor.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace app\data\extractors;
+
+use epf\epfString;
+use stdClass;
+
+class ChangerDataExtractor extends AbstractDataExtractor
+{
+    protected const TYPE = 'Change';
+    private $extractorName = 'changes';
+
+    protected function createAttributesComponents(): void
+    {
+        $extractorName = $this->extractorName();
+        $arrayChange   = [];
+
+        foreach($this->data->$extractorName->attributes as $attribute) {
+            $change             = new stdClass();
+            $epfStringChange    = new epfString($attribute);
+            $epfStringChanges   = $epfStringChange->split('-');
+            $epfStringComponent = new epfString($epfStringChanges[0]->__toString());
+            $componentsArray    = $epfStringComponent->split(',');
+            $change->components = $this->extractComponents($componentsArray);
+            $filesArray         = $epfStringChanges[1]->split(',');
+            $change->files      = $this->extractFiles($filesArray);
+
+            $arrayChange[] = $change;
+        }
+
+        $this->data->changes = $arrayChange;
+    }
+
+    public function extractorName(): string
+    {
+        return $this->extractorName;
+    }
+    public function extractData(): stdClass
+    {
+        return $this->data;
+    }
+
+    private function extractComponents(array $componentsArray): stdClass
+    {
+        $changeComponents = new stdClass();
+        foreach ($componentsArray as $component) {
+            $changeComponents->component[] = $component->__toString();
+        }
+
+        return $changeComponents;
+    }
+
+    private function extractFiles(array $filesArray): stdClass
+    {
+        $headersExtension         = ['h'];
+        $implementationsExtension = ['c', 'cpp'];
+        $files                    = new stdClass();
+
+        foreach ($filesArray as $file ) {
+
+            $fileArrayToSplit      = new epfString($file->__toString());
+            $fileArray             = $fileArrayToSplit->split(':');
+            $fileNameWithExtension = $fileArray[0]->__toString();
+            $changedLines          = $fileArray[1]->__toString();
+
+            $epfStringFileNameWithExtension = new epfString($fileNameWithExtension);
+            $arrayFileNameWithExtension     = $epfStringFileNameWithExtension->split('.');
+
+            $extension = $arrayFileNameWithExtension[1]->__toString();
+
+
+            if (in_array($extension, $headersExtension, true)) {
+                $files->headers['file'][] = [
+                    'name' => $fileNameWithExtension,
+                    'hangedLines' => $changedLines
+                ];
+            }
+
+            if (in_array($extension, $implementationsExtension, true)) {
+                $files->implementations['file'][] = [
+                    'name' =>$fileNameWithExtension,
+                    'changedLines' =>  $changedLines
+                ];
+
+            }
+
+        }
+
+        return $files;
+    }
+}
--- php/www/app/data/extractors/HeaderDataExtractor.php
+++ php/www/app/data/extractors/HeaderDataExtractor.php
@@ -2,59 +2,71 @@
 
 namespace app\data\extractors;
 
+use app\adapter\epfStringParser;
 use epf\epfDateTime;
 use epf\epfString;
 
 class HeaderDataExtractor extends AbstractDataExtractor
 {
-
-    protected const HEADER_TYPE = 'Header';
-    protected const NODE_TYPES = [
-        self::HEADER_TYPE => 'header'
-    ];
+    protected const TYPE = 'Header';
 
     private $extractorName = 'header';
     private $autoApprove   = '6 years';
 
     protected function createAttributesComponents(): void
     {
-        $nodeType = $this->extractorName;
+        $extractorName = $this->extractorName();
 
-        $data = new epfString($this->data[$nodeType][0]);
+        $objectParserWriter = new epfStringParser($this->data->$extractorName->attributes);
 
-        $pairs = $data->split(';');
+        $attributes = $objectParserWriter->toArray();
+        $attributesArray = $this->applyBusinessRuleToAttributes($attributes);
 
+        $this->data->$extractorName->attributes = (object) $attributesArray;
+    }
+
+    public function extractorName(): string
+    {
+        return $this->extractorName;
+    }
+
+    public function extractData(): object
+    {
+        return $this->data;
+    }
+
+    private function applyBusinessRuleToAttributes(array $attributes): array
+    {
         $array = [];
-
-        foreach($pairs as $pair) {
-
-            [$key, $value] = (new epfString($pair->__toString()))->split('=');
-
-            $key->toLower();
-
-            $key = $key->__toString();
+        foreach($attributes as $key => $value) {
 
             if (!isset($value)) {
                 continue;
             }
 
-            if ($key !== 'author') {
-                $value->toLower();
+            $epfStringKey = new epfString($key);
+            $epfStringKey->toLower();
+            $newKey = $epfStringKey->__toString();
+
+            $epfStringValue = new epfString($value);
+
+            if ($newKey !== 'author') {
+                $epfStringValue->toLower();
             }
 
-            $value = $value->__toString();
+            $newValue = $epfStringValue->__toString();
 
-            if ($key === 'timestamp') {
+            if ($newKey === 'timestamp') {
 
-                $epfDateTime = new epfDateTime($value);
+                $epfDateTime = new epfDateTime($newValue);
                 $epfDateTime->format('Y-m-d H:i:s');
 
-                $array[$key] = $epfDateTime->format('Y-m-d H:i:s');
+                $array[$newKey] = $epfDateTime->format('Y-m-d H:i:s');
 
                 continue;
             }
 
-            if ($key === 'autoapprove') {
+            if ($newKey === 'autoapprove') {
 
                 $timestamp = new epfDateTime($array['timestamp']);
 
@@ -63,7 +75,7 @@ class HeaderDataExtractor extends AbstractDataExtractor
                 continue;
             }
 
-            $array[$key] = $value;
+            $array[$newKey] = $newValue;
         }
 
         if (!isset($array['signed']))
@@ -71,19 +83,6 @@ class HeaderDataExtractor extends AbstractDataExtractor
             $array['signed'] = 'no';
         }
 
-        $this->data[$nodeType] = $array;
-    }
-
-    public function extractorName(): string
-    {
-        return $this->extractorName;
-    }
-
-    /**
-     * return ['Author=Bill Gates, Timestamp=853082238, signed=Yes, AutoApprove=12 hours]
-     */
-    public function extractData(): array
-    {
-        return $this->data[$this->extractorName];
+        return $array;
     }
 }
